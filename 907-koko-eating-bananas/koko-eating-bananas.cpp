a44d5afff4db29e89a076f2ddf7f20ae2cd81851
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-06 16:37:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (76.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -11,9 +11,9 @@ public:
 
         int low = 1;
         int high = max;
+        int ans = max;
 
-
-        while(low<high){
+        while(low<=high){
             long long hours = 0;
 
             int mid = low + (high - low)/2;
@@ -23,12 +23,13 @@ public:
             }
 
             if(hours<=h){
-                high = mid;
+                ans = mid;
+                high = mid - 1;
             }
             else{
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 };
