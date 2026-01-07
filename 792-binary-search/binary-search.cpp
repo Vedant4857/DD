f848a7c5810cb70bf85d72d600529df4a728eb6e
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-07 11:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.27%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -7,9 +7,9 @@ public:
 
         while(low<=high){
             int mid = low + (high - low)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high = mid -1;
-            else low = mid+1;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
         }
         return -1;
     }
