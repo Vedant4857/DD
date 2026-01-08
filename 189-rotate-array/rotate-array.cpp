b2a46bc060a8eeb899714dae92332e5f323ff00e
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-08 13:45:58 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (64.49%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int i = 0 , j = n-1;
+        k = k%n;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+
+        i = 0, j = k-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+
+        i = k, j = n-1;
+        while(i<j){
+            swap(nums[i],nums[j]);
+            i++;
+            j--;
+        }
+
+    }
+};
