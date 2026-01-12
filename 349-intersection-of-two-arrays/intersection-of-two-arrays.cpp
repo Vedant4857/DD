0ab3ee8fa7745062024d7dc4135a7bede5c5827c
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-12 15:54:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (63.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<int> nums3;
-
-        unordered_set <int> vedant;
-        for(int i = 0;i<m;i++){
-            if(vedant.count(nums1[i])) continue;
-            vedant.insert(nums1[i]);
-        }
-        for(int i = 0;i<n;i++){
-            if(vedant.count(nums2[i])){
-                nums3.push_back(nums2[i]);
-                vedant.erase(nums2[i]);
-            }
-        }
-        return nums3;
+       set <int> vedant;
+       sort(nums1.begin(), nums1.end());
+       for(int x : nums2){
+        if(binary_search(nums1.begin(),nums1.end(),x)) vedant.insert(x);
+       }
+       return vector<int>(vedant.begin(),vedant.end());
     }
 };
