e7652ab9e9dcf78e7528840f2eb76bca5aa86582
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-16 21:56:27 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (53.37%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return 0;
+        int i = 0;
+        int sum = 0;
+        int totalsum = accumulate(nums.begin(),nums.end(),0);
+        while(i<n){
+            if(totalsum - nums[i] - sum == sum){
+                return i;
+            }
+            sum += nums[i];
+            i++;
+        }
+        return -1;
+    }
+};
