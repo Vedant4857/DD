edbc5cd6143620030166b5394d0adc1cbbb680a8
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-28 22:11:14 +0530

    Time: 3 ms (40.57%) | Memory: 8.3 MB (86.76%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        int n = s.size();
+        string ans = "";
+        int num = 1;
+        while (num <= 9){
+            int j = n-1;
+            while(j>=0 && s[j] != num + '0'){
+                j--;
+            }
+            j--;
+            string xyz = "";
+            if (j < 0) break;
+
+            while(j>=0 && s[j]!=' '){
+                xyz += s[j];
+                j--;
+            }
+            reverse(xyz.begin(),xyz.end());
+            if (!ans.empty()) ans += ' ';
+            ans += xyz;
+            num++;
+        }
+        return ans;
+    }
+};
