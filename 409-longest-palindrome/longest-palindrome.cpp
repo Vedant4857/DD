2b8fb35a00f60fe36a026011933afd3bc755a7f8
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-01-29 00:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (81.87%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-    vector<int> freq(256);
-    int ans = 0;
-    bool odd = false;
-    for(int i =0;i<s.size();i++){
-        freq[s[i]]++;
-    }
+        vector<int> freq(256);
+        int ans = 0;
+        bool odd = false;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i]]++;
+        }
 
-    for(int i =0;i<256;i++){
-        if(freq[i]%2==0){
-            ans += freq[i];
+        for (int i = 0; i < 256; i++) {
+            if (freq[i] % 2 == 0) {
+                ans += freq[i];
+            } else {
+                ans += freq[i] - 1;
+                odd = true;
+            }
         }
-        else{
-            ans += freq[i]-1;
-            odd = true;
-        }
+        if (odd)
+            ans = ans + 1;
+        return ans;
     }
-    if(odd) ans = ans + 1;
-    return ans;
-}
 };
