2a68e81f7b33b2a80e1a1a3c5fb08ec92a1db8a9
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-02-14 10:46:32 +0530

    Time: 11 ms (95.86%) | Memory: 52.1 MB (92.69%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-
-    int atMostK(vector<int>& nums, int k) {
-        if (k < 0) return 0;
-
-        int n = nums.size();
-        vector<int> freq(20001, 0);   // adjust size if constraint differs
-
+    int withmaxK(vector<int>& arr, int k){
         int left = 0, distinct = 0;
+        vector<int> freq(20001,0);
         int ans = 0;
 
-        for (int right = 0; right < n; right++) {
+        for(int right = 0;right < arr.size();right++){
+            if(freq[arr[right]] == 0) distinct++;
+            freq[arr[right]]++;
 
-            if (freq[nums[right]] == 0)
-                distinct++;
-
-            freq[nums[right]]++;
-
-            while (distinct > k) {
-                freq[nums[left]]--;
-                if (freq[nums[left]] == 0)
-                    distinct--;
+            while(distinct > k){
+                freq[arr[left]]--;
+                if(freq[arr[left]] == 0) distinct--;
                 left++;
             }
-
-            ans += (right - left + 1);
+            ans += (right - left + 1); 
         }
-
         return ans;
     }
-
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
+        return withmaxK(nums,k) - withmaxK(nums,k-1);
     }
 };
