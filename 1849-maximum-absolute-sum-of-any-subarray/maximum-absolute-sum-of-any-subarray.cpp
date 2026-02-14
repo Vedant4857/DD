85c0bbac0c46fa6662f799cfdc993f7eef0e2b2c
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-02-14 18:30:00 +0530

    Time: 3 ms (19.59%) | Memory: 45 MB (87.02%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -5,22 +5,18 @@ public:
         long long mini = INT_MAX;
         long long sum1 = 0, sum2 = 0;
         int n = nums.size();
-        int i = 0;
 
-        while(i<n){
+        for (int i = 0; i < n; i++) {
             sum1 += nums[i];
-            if(sum1<0) sum1 = 0;
-            maxi = max(sum1,maxi);
+            if (sum1 < 0)
+                sum1 = 0;
+            maxi = max(sum1, maxi);
 
             sum2 += nums[i];
-            if(sum2>0) sum2 = 0;
-            mini = min(sum2,mini);
-
-            i++;
+            if (sum2 > 0)
+                sum2 = 0;
+            mini = min(sum2, mini);
         }
-        return max(maxi,abs(mini));
-
-        
-
+        return max(maxi, abs(mini));
     }
 };
