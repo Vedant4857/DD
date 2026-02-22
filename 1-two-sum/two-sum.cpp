6ca4eadbcd1de4b9f7ca13ef2366fbb7b7d0927d
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-02-22 11:14:35 +0530

    Time: 6 ms (43.99%) | Memory: 14.8 MB (52.00%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        unordered_map <int,int> vedant;
-        for(int i =0;i<n;i++){
-            int z = target - nums[i];
-            if(vedant.count(z)){
-                return {vedant[z],i};
-            }
+    unordered_map <int,int> vedant;
+    int n = nums.size();
+
+    for(int i =0;i<n;i++){
+        int target2 = target-nums[i];
+
+        if(vedant.count(target2)){
+            return {vedant[target2],i};
+        }
+        else{
             vedant[nums[i]] = i;
         }
-        return {};
     }
+    return {};
+
+}
 };
