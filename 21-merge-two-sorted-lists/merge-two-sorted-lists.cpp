f08c012c3f422162436890ba3f9336f8d118937e
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-12 21:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (98.95%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,18 +11,57 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1) return list2;
-        if(!list2) return list1;
+        if (!list1)
+            return list2;
+        if (!list2)
+            return list1;
 
-        ListNode * c;
-        if(list1->val <= list2->val){
+        ListNode* c;
+        if (list1->val <= list2->val) {
             c = list1;
-            c->next = mergeTwoLists(list1->next,list2);
-        }
-        else{
+            c->next = mergeTwoLists(list1->next, list2);
+        } else {
             c = list2;
-            c->next = mergeTwoLists(list1,list2->next);
+            c->next = mergeTwoLists(list1, list2->next);
         }
         return c;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode* temp = list1;
+//         ListNode* Head = new ListNode(0);
+//         ListNode* Tail = Head;
+
+//         while (list1 && list2) {
+//             if (list1->val <= list2->val) {
+//                 Tail->next = list1;
+//                 list1 = list1->next;
+//                 Tail = Tail->next;
+//                 Tail->next = NULL;
+//             } else {
+//                 Tail->next = list2;
+//                 list2 = list2->next;
+//                 Tail = Tail->next;
+//                 Tail->next = NULL;
+//             }
+//         }
+//         while (list1) {
+//             Tail->next = list1;
+//             list1 = list1->next;
+//             Tail = Tail->next;
+//             Tail->next = NULL;
+//         }
+//         while (list2) {
+//             Tail->next = list2;
+//             list2 = list2->next;
+//             Tail = Tail->next;
+//             Tail->next = NULL;
+//         }
+
+//         Head = Head->next;
+//         return Head;
+//     }
+// };
