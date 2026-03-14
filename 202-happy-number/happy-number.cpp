2d2d85abf26ee20066a656d720d99ccbe91716c1
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-14 12:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (96.23%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1 || n==7) return true;
+        if(n<10) return false;
+        int sum = 0;
+
+        while(n>0){
+            int x = n%10;
+            sum += x * x;
+            n = n/10;
+        }
+        return isHappy(sum);
+    }
+};
