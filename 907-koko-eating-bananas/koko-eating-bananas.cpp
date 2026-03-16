ccc202ca2ab006409ef66f614efdb60fa3a0fd31
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-16 22:37:52 +0530

    Time: 8 ms (54.27%) | Memory: 23 MB (45.82%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    bool canEat(vector<int>& piles, int mid, int h) {
-        long long sum = 0;
-        int n = piles.size();
+    bool vedant(vector<int>& arr, int mid, int h) {
+        int n = arr.size();
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            sum += (piles[i] + mid - 1) / mid;
+            sum += (arr[i] + mid - 1) / mid;
         }
         return (sum <= h);
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
-        int ans = high;
 
-        while (low <= high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
 
-            if (canEat(piles, mid, h)) {
-                ans = mid;
-                high = mid - 1;
-            } else
+            if (vedant(piles, mid, h)) {
+                high = mid;
+            } else {
                 low = mid + 1;
+            }
         }
-        return ans;
+        return high;
     }
 };
