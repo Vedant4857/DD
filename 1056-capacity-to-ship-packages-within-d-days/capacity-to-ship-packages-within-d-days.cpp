e775a46bb1c61b73939dc7f30161b78f10386fb9
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-16 23:48:51 +0530

    Time: 11 ms (67.17%) | Memory: 35 MB (54.02%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int canShip(vector<int>& weights, int mid, int days) {
+    bool vedant(vector<int>& arr, int mid, int k) {
+        int n = arr.size();
         int sum = 0;
-        int day = 1;
-
-        for (int i = 0; i < weights.size(); i++) {
-            if (sum + weights[i] <= mid) {
-                sum += weights[i];
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            if (sum + arr[i] <= mid) {
+                sum += arr[i];
             } else {
-                if (day < days) {
-                    day++;
-                    sum = weights[i];
-                } else
-                    return 0;
+                count++;
+                sum = arr[i];
             }
         }
-        return 1;
+        return (count <= k);
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int low = *max_element(weights.begin(), weights.end());
         int high = accumulate(weights.begin(), weights.end(), 0);
-        int ans = high;
 
-        while (low <= high) {
+        while (low < high) {
             int mid = low + (high - low) / 2;
 
-            if (canShip(weights, mid, days)) {
-                ans = mid;
-                high = mid - 1;
-            } else
+            if (vedant(weights, mid, days)) {
+                high = mid;
+            } else {
                 low = mid + 1;
+            }
         }
-        return ans;
+        return high;
     }
 };
