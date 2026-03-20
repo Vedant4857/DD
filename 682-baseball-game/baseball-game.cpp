804d27e33b530dfffeae3ff2b6c448112aae7ae0
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-20 20:43:10 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (30.35%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    bool isNumber(string s) {
-        if (s.empty())
-            return false;
-        int i = 0;
-        if (s[0] == '-')
-            i = 1;
-        return i < s.size() && all_of(s.begin() + i, s.end(), ::isdigit);
-    }
     int calPoints(vector<string>& o) {
         stack<int> st;
         for (int i = 0; i < o.size(); i++) {
-            if (isNumber(o[i])) {
-                st.push(stoi(o[i]));
+            if (o[i] == "C") {
+                st.pop();
+            } else if (o[i] == "D") {
+                st.push(st.top() * 2);
+            } else if (o[i] == "+") {
+                int x = st.top();
+                st.pop();
+                int sum = x + st.top();
+                st.push(x);
+                st.push(sum);
             } else {
-                if (o[i] == "C") {
-                    st.pop();
-                } else if (o[i] == "D") {
-                    st.push(st.top() * 2);
-                } else if (o[i] == "+") {
-                    int x = st.top();
-                    st.pop();
-                    int sum = x + st.top();
-                    st.push(x);
-                    st.push(sum);
-                }
+                st.push(stoi(o[i]));
             }
         }
         int sum = 0;
