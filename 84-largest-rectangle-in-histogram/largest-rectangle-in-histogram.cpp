bf0453dc736b659e337979aae59ca6e2b31494e8
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-21 23:43:09 +0530

    Time: 21 ms (61.38%) | Memory: 81.4 MB (60.27%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -3,31 +3,20 @@ public:
     int largestRectangleArea(vector<int>& heights) {
         stack<int> st;
         int n = heights.size();
-        vector<int> NSR(n);
-        vector<int> NSL(n, -1);
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
-                NSR[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while (!st.empty()) {
-            NSR[st.top()] = n;
-            st.pop();
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && heights[i] < heights[st.top()]) {
-                NSL[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        }
         int maxarea = 0;
-        for (int i = 0; i < n; i++) {
-            maxarea = max(maxarea, (NSR[i] - NSL[i] - 1) * heights[i]);
+
+        for (int i = 0; i <= n; i++) {
+            int currheight = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && currheight < heights[st.top()]) {
+                int height = heights[st.top()];
+                st.pop();
+                int NSR = i;
+                int NSL = st.empty() ? -1 : st.top();
+                int width = NSR - NSL - 1;
+                maxarea = max(maxarea, height * width);
+            }
+            st.push(i);
         }
         return maxarea;
     }
