74bdf2079ad838a0c98c8503788f9d2bf20dd663
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-21 22:33:41 +0530

    Time: 36 ms (16.46%) | Memory: 89.3 MB (12.18%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,54 +1,34 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-    
+        stack<int> st;
+        int n = heights.size();
+        vector<int> NSR(n);
+        vector<int> NSL(n, -1);
 
-    stack<int>st;
-    int n = heights.size();
-
-    // Next smallest Right and Left
-
-    vector<int>NSR(n);
-    vector<int>NSL(n,-1);
-
-
-    for(int i=0;i<n;i++){
-        while(!st.empty()&&heights[i]<heights[st.top()]){
-            NSR[st.top()] = i;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[i] < heights[st.top()]) {
+                NSR[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        while (!st.empty()) {
+            NSR[st.top()] = n;
             st.pop();
         }
 
-        st.push(i);
-    }
-
-    while(!st.empty()){
-        NSR[st.top()] = n;
-        st.pop();
-    }
-
-
-    // Next smallest element on Left side
-
-    for(int i=n-1;i>=0;i--){
-        while(!st.empty()&&heights[i]<heights[st.top()]){
-            NSL[st.top()] = i;
-            st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[i] < heights[st.top()]) {
+                NSL[st.top()] = i;
+                st.pop();
+            }
+            st.push(i);
         }
-
-        st.push(i);
+        int maxarea = 0;
+        for (int i = 0; i < n; i++) {
+            maxarea = max(maxarea, (NSR[i] - NSL[i] - 1) * heights[i]);
+        }
+        return maxarea;
     }
-
-
-    int maxArea = 0;
-
-    for(int i=0;i<n;i++){
-        maxArea = max(maxArea,(NSR[i]-NSL[i]-1)*heights[i]);
-    }
-
-
-    return maxArea;
-
-
-
-}
 };
