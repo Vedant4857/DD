f7832bb0cbeee3034443a5fa6bbe01e18ab48af9
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-25 20:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (97.75%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,91 +1,27 @@
 class MyStack {
-    public:
-    queue<int>q1;
-    queue<int>q2;
-    MyStack() {
-
-    }
+public:
+    queue<int> q;
+    MyStack() {}
 
     void push(int x) {
-        if(!q1.empty()){
-            q1.push(x);
-        }
-        else{
-            q2.push(x);
+        q.push(x);
+        int n = q.size();
+        for (int i = 0; i < n - 1; i++) {
+            q.push(q.front());
+            q.pop();
         }
     }
 
     int pop() {
-        if(!q1.empty()){
-
-            while(q1.size()>1){
-                q2.push(q1.front());
-                q1.pop();
-            }
-
-            int number = q1.front();
-            q1.pop();
-            return number;
-
-        }
-        else{
-
-            while(q2.size()>1){
-                q1.push(q2.front());
-                q2.pop();
-            }
-
-            int number = q2.front();
-            q2.pop();
-            return number;
-        }
+        int r = q.front();
+        q.pop();
+        return r;
     }
 
-    int top() {
-        if(!q1.empty()){
-            return q1.back();
-        }
-        else{
-            return q2.back();
-        }
-    }
+    int top() { return q.front(); }
 
-    bool empty() {
-        return q1.empty()&&q2.empty();
-
-    }
+    bool empty() { return q.empty(); }
 };
-
-
-
-
-
-
-
-// class MyStack {
-// public:
-//     queue<int> q;
-//     MyStack() {}
-
-//     void push(int x) {
-//         q.push(x);
-//         int n = q.size();
-//         for (int i = 0; i < n - 1; i++) {
-//             q.push(q.front());
-//             q.pop();
-//         }
-//     }
-
-//     int pop() {
-//         int r = q.front();
-//         q.pop();
-//         return r;
-//     }
-
-//     int top() { return q.front(); }
-
-//     bool empty() { return q.empty(); }
-// };
 
 /**
  * Your MyStack object will be instantiated and called as such:
@@ -95,3 +31,62 @@ class MyStack {
  * int param_3 = obj->top();
  * bool param_4 = obj->empty();
  */
+
+// USING TWO QUEUE::::
+//  class MyStack {
+//      public:
+//      queue<int>q1;
+//      queue<int>q2;
+//      MyStack() {
+
+//     }
+
+//     void push(int x) {
+//         if(!q1.empty()){
+//             q1.push(x);
+//         }
+//         else{
+//             q2.push(x);
+//         }
+//     }
+
+//     int pop() {
+//         if(!q1.empty()){
+
+//             while(q1.size()>1){
+//                 q2.push(q1.front());
+//                 q1.pop();
+//             }
+
+//             int number = q1.front();
+//             q1.pop();
+//             return number;
+
+//         }
+//         else{
+
+//             while(q2.size()>1){
+//                 q1.push(q2.front());
+//                 q2.pop();
+//             }
+
+//             int number = q2.front();
+//             q2.pop();
+//             return number;
+//         }
+//     }
+
+//     int top() {
+//         if(!q1.empty()){
+//             return q1.back();
+//         }
+//         else{
+//             return q2.back();
+//         }
+//     }
+
+//     bool empty() {
+//         return q1.empty()&&q2.empty();
+
+//     }
+// };
