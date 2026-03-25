f8aa5a26e51b2819752d60a27dd2f3eb90aacff8
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-03-25 20:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (13.94%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,27 +1,91 @@
 class MyStack {
-public:
-    queue<int> q;
-    MyStack() {}
+    public:
+    queue<int>q1;
+    queue<int>q2;
+    MyStack() {
+
+    }
 
     void push(int x) {
-        q.push(x);
-        int n = q.size();
-        for (int i = 0; i < n - 1; i++) {
-            q.push(q.front());
-            q.pop();
+        if(!q1.empty()){
+            q1.push(x);
+        }
+        else{
+            q2.push(x);
         }
     }
 
     int pop() {
-        int r = q.front();
-        q.pop();
-        return r;
+        if(!q1.empty()){
+
+            while(q1.size()>1){
+                q2.push(q1.front());
+                q1.pop();
+            }
+
+            int number = q1.front();
+            q1.pop();
+            return number;
+
+        }
+        else{
+
+            while(q2.size()>1){
+                q1.push(q2.front());
+                q2.pop();
+            }
+
+            int number = q2.front();
+            q2.pop();
+            return number;
+        }
     }
 
-    int top() { return q.front(); }
+    int top() {
+        if(!q1.empty()){
+            return q1.back();
+        }
+        else{
+            return q2.back();
+        }
+    }
 
-    bool empty() { return q.empty(); }
+    bool empty() {
+        return q1.empty()&&q2.empty();
+
+    }
 };
+
+
+
+
+
+
+
+// class MyStack {
+// public:
+//     queue<int> q;
+//     MyStack() {}
+
+//     void push(int x) {
+//         q.push(x);
+//         int n = q.size();
+//         for (int i = 0; i < n - 1; i++) {
+//             q.push(q.front());
+//             q.pop();
+//         }
+//     }
+
+//     int pop() {
+//         int r = q.front();
+//         q.pop();
+//         return r;
+//     }
+
+//     int top() { return q.front(); }
+
+//     bool empty() { return q.empty(); }
+// };
 
 /**
  * Your MyStack object will be instantiated and called as such:
