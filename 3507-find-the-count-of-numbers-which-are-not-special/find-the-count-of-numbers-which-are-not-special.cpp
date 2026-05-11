eb163c1583e1840b7879e383d13fec201bd404a1
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-05-11 18:05:36 +0530

    Time: 49 ms (34.00%) | Memory: 8.6 MB (85.00%) - LeetSync

--- 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isPrime(int n) {
-    if (n <= 1) return false;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0)
+        if (n <= 1)
             return false;
-    }
 
-    return true;
-}
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
     int nonSpecialCount(int l, int r) {
         int s = r - l + 1;
-        //must be square of a prime number
+        // must be square of a prime number
         int le = sqrt(l);
         int ri = sqrt(r);
 
-        for(int i = le;i<=ri;i++){
-            if(isPrime(i) && i*i<=r && i*i>=l){
+        for (int i = le; i <= ri; i++) {
+            if (isPrime(i) && i * i <= r && i * i >= l) {
                 s--;
             }
         }
