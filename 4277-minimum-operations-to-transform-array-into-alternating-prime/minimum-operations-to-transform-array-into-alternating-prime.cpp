125862534de27a255308fbb27d1ef1cdfcb6adf0
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-05-12 20:55:51 +0530

    Time: 288 ms (59.09%) | Memory: 116.8 MB (68.96%) - LeetSync

---
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isprime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        for (int i = 2; i*i <=x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                while (!isprime(nums[i])) {
+                    nums[i] += 1;
+                    count++;
+                }
+            } else {
+                while (isprime(nums[i])) {
+                    nums[i] += 1;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
