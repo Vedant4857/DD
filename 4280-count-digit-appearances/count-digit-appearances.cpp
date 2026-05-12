7ff80d40d34bc17ea88476119cefd6cc413b22eb
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-05-12 20:00:08 +0530

    Time: 12 ms (10.81%) | Memory: 37.9 MB (9.72%) - LeetSync

--- 4280-count-digit-appearances/count-digit-appearances.cpp
+++ 4280-count-digit-appearances/count-digit-appearances.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int countDigitOccurrences(vector<int>& nums, int digit) {
         int count = 0;
-        for(int i = 0;i<nums.size();i++){
+        for (int i = 0; i < nums.size(); i++) {
             string x = to_string(nums[i]);
             int j = 0;
             char dig = 48 + digit;
-            while(j<x.size()){
-                if(x[j]==dig){
+            while (j < x.size()) {
+                if (x[j] == dig) {
                     count++;
                 }
                 j++;
