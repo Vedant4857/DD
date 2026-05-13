883b753af5c2a7d7dfb022534f5274f324f1217d
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-05-13 12:48:54 +0530

    Time: 3 ms (69.13%) | Memory: 9.2 MB (34.62%) - LeetSync

---
+++ 4297-sum-of-primes-between-number-and-its-reverse/sum-of-primes-between-number-and-its-reverse.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<bool> sieve(int n) {
+        vector<bool> s(n + 1, true);
+        if (n >= 0)
+            s[0] = 0;
+        if (n >= 1)
+            s[1] = 0;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (s[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    s[j] = 0;
+                }
+            }
+        }
+        return s;
+    }
+    int sumOfPrimesInRange(int n) {
+        int x = n;
+        int sum = 0;
+        while (x>=1) {
+            int y = x % 10;
+            sum = sum * 10 + y;
+            x = x / 10;
+        }
+        int mini = min(n,sum);
+        int maxi = max(n, sum);
+        vector<bool> primee = sieve(maxi);
+        
+        int sumo = 0;
+        for(int i = mini;i<=maxi;i++){
+            if(primee[i]){
+                sumo += i;
+            }
+        }
+        return sumo;
+    }
+};
