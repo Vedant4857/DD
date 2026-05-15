64b527a0fd8aab5e58658868c5863d98c345c782
Vedant4857 <153383069+Vedant4857@users.noreply.github.com>
2026-05-15 17:18:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.64%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int minCost(int n) {
-        int cost = 0;
-        while(n>=1){
-            int x = 1;
-            int y = n - 1;
-            n = x * y;
-            cost += n;
-        }
-        return cost;
+        return n*(n-1)/2;
     }
 };
