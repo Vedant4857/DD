89a47bc27c7d01a779931d1439a1d2dca689d53c
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-18 20:15:45 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (82.21%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int atmost(vector<int>& nums, int goal){
-        if (goal < 0) return 0;
-        int left = 0, right = 0, n = nums.size(),sum = 0,ans=0;
+    int atmost(vector<int>& nums, int goal) {
+        if (goal < 0)
+            return 0;
+        int left = 0, right = 0, n = nums.size(), sum = 0, ans = 0;
 
-        while(right<n){
+        while (right < n) {
             sum += nums[right];
 
-            while(sum>goal){
+            while (sum > goal) {
                 sum -= nums[left];
                 left++;
             }
-            ans += (right - left +1);
+            ans += (right - left + 1);
             right++;
         }
         return ans;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return atmost(nums,goal) - atmost(nums,goal-1);
+        return atmost(nums, goal) - atmost(nums, goal - 1);
         // int left = 0, right = 0, n = nums.size(),sum = 0,ans=0;
 
         // while(right<n){
