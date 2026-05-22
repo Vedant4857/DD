b4180444383af8ac1fc049a1b3b9da2b749ab891
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-22 22:38:01 +0530

    Time: 7 ms (21.13%) | Memory: 18.1 MB (47.45%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    class cmp{
+        public:
+        bool operator()(pair<string,int>&a,pair<string,int>&b){
+            if(a.second==b.second){
+                return a.first < b.first;
+            }
+            return a.second>b.second;
+        }
+    };
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        // pair<string,int>
+        priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> pq;
+        unordered_map<string, int> mp;
+        vector<string> ans;
+        for (auto w : words) {
+            mp[w]++;
+        }
+
+        for (auto it : mp) {
+            pq.push({it.first, it.second});
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        while (!pq.empty()) {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
