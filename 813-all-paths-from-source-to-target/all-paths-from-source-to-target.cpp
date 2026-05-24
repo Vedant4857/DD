fa50bd28023ca3c0a1c963d66f2b7d0cef410c71
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-24 15:46:59 +0530

    Time: 14 ms (18.29%) | Memory: 19.5 MB (15.38%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void DFS(vector<vector<int>>& graph, vector<vector<int>>& ans,
+             vector<int> subans, int sour, int dest) {
+        subans.push_back(sour);
+        if (sour == dest) {
+            ans.push_back(subans);
+        }
+        for (int neib : graph[sour]) {
+            DFS(graph, ans, subans, neib, dest);
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int v = graph.size();
+        vector<vector<int>> ans;
+        vector<int> subans;
+        DFS(graph, ans, subans, 0, v - 1);
+        return ans;
+    }
+};
