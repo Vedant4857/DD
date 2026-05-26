69f57e245db55f320908a3d5adf9fca3111675c4
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-26 11:50:12 +0530

    Time: 17 ms (67.74%) | Memory: 63.9 MB (8.60%) - LeetSync

--- 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
+++ 2845-find-the-value-of-the-partition/find-the-value-of-the-partition.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         int mini = INT_MAX;
         for (int i = 1; i < n; i++) {
-            mini = min(mini, nums[i] - nums[i - 1]);
+            mini = min(mini, abs(nums[i] - nums[i - 1]));
         }
         return mini;
     }
