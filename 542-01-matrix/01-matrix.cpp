eea3c5f0ba578351075786924fab06da5f6e5c70
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-28 12:49:32 +0530

    Time: 9 ms (87.56%) | Memory: 34.6 MB (92.33%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isvalid(int i, int j, int row, int col) {
+        if (i < row && j < col && i >= 0 && j >= 0) {
+            return true;
+        }
+        return 0;
+    }
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        // 0. 1.  0.  1.  0.
+        // 1. 0.  1.  1.  1
+        // 1. 1.  1.  0.  0
+        // 0. 1.  0.  1.  0
+        // 1. 1.  1.  1.  1
+        // 0. 0.  0.  1.  1
+
+        // ietarte and find all 1 and put in queue;
+        int row = mat.size();
+        int col = mat[0].size();
+        queue<pair<int, int>> q;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (mat[i][j] == 0) {
+                    q.push({i, j});
+                } else {
+                    mat[i][j] = -1;
+                }
+            }
+        }
+
+        while (!q.empty()) {
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            int r[4] = {0, 0, 1, -1};
+            int c[4] = {1, -1, 0, 0};
+
+            for (int k = 0; k < 4; k++) {
+                if (isvalid(i + r[k], j + c[k], row, col) &&
+                    mat[i + r[k]][j + c[k]] == -1) {
+                    mat[i + r[k]][j + c[k]] = mat[i][j] + 1;
+                    q.push({i + r[k], j + c[k]});
+                }
+            }
+        }
+        return mat;
+    }
+};
