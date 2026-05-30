5d33dfa909ebb4cd33a906738fb5fa5d3a02691c
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-30 15:41:41 +0530

    Time: 4 ms (36.36%) | Memory: 18.1 MB (21.36%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    void vedant(vector<int>& c,int index, int n, int sum,vector<int> &subans,vector<vector<int>> &ans, int target){
-
-        if(index==n || sum>target){
-            if(sum==target){
-                ans.push_back(subans);
-            }
+    void vedant(vector<int>& c, vector<int>& subans, vector<vector<int>>& ans,
+                int index, int sum, int n, int target) {
+        if (sum == target) {
+            ans.push_back(subans);
             return;
         }
-        subans.push_back(c[index]);
-        vedant(c,index,n,sum+c[index],subans,ans,target);
-        subans.pop_back();
-        vedant(c,index+1,n,sum,subans,ans,target);
-    }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int>subans;
-        vedant(candidates,0,candidates.size(),0,subans,ans,target);
 
+        if (sum > target || index == n) {
+            return;
+        }
+
+        vedant(c, subans, ans, index + 1, sum, n, target);
+        subans.push_back(c[index]);
+        vedant(c, subans, ans, index, sum + c[index], n, target);
+        subans.pop_back();
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+        vector<int> subans;
+        vector<vector<int>> ans;
+        int n = c.size();
+        vedant(c, subans, ans, 0, 0, n, target);
         return ans;
     }
 };
