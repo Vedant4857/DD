012ad191d3f518af52bd36820b5f7c71ce4301b0
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-30 16:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (53.95%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void vedant(vector<int>& nums, int index, int n, vector<vector<int>>& ans) {
-        if(index==n-1){
+    void vedant(vector<int>& nums, int index, vector<vector<int>>& ans, int n) {
+
+        if (index == n) {
             ans.push_back(nums);
             return;
         }
-        vector<bool> used(21,0);
+
+        vector<bool> used(21, 0);
+
         for (int i = index; i < n; i++) {
-            if(used[nums[i]+10] == 1){
-                continue;
+            if (!used[nums[i] + 10]) {
+                used[nums[i]+10] = 1;
+                swap(nums[i], nums[index]);
+                vedant(nums, index + 1, ans, n);
+                swap(nums[i], nums[index]);
             }
-            used[nums[i]+10] = 1;
-            swap(nums[index], nums[i]);
-            vedant(nums, index + 1, n, ans);
-            swap(nums[index], nums[i]);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
         vector<vector<int>> ans;
-        vedant(nums, 0, nums.size(), ans);
+        vedant(nums, 0, ans, n);
         return ans;
     }
 };
