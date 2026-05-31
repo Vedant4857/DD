2c2a76f06e4ecb14022538825305f33975bed336
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-05-31 12:57:34 +0530

    Time: 6 ms (13.19%) | Memory: 16.3 MB (18.23%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void vedant(int n, int open, int close, string &x, vector<string>& ans) {
+    void vedant(int n, int open, int close, string x, vector<string>& ans) {
         if (open == n && close == n) {
             ans.push_back(x);
             return;
         }
-        if (open < n) {
-            x.push_back('(');
-            vedant(n, open + 1, close, x, ans);
-            x.pop_back();
+
+        if (close > open || open > n) {
+            return;
         }
-        if (close < open) {
-            x.push_back(')');
-            vedant(n, open, close + 1, x, ans);
-            x.pop_back();
-        }
+
+        vedant(n, open + 1, close, x + '(', ans);
+        vedant(n, open, close + 1, x + ')', ans);
     }
     vector<string> generateParenthesis(int n) {
+        string x = "";
         vector<string> ans;
-        string x;
         vedant(n, 0, 0, x, ans);
         return ans;
     }
