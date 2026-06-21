eda12f417359c52f1bd1f6980b342c7c0a53a745
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-06-21 12:47:59 +0530

    Time: 22 ms (74.72%) | Memory: 108.6 MB (5.01%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int count = 0;
-        sort(costs.begin(), costs.end());
+        vector<int> prices(100001,0);
+        // unordered_map<int,int>freq;
+
         for (int x : costs) {
-            coins -= x;
-            if (coins < 0) {
-                return count;
-            }
-            count++;
+            prices[x]++;
+        }
+        int count = 0;
+        // number of icecream of that price
+        for (int i = 1; i <= 100000; i++) {
+            int canbuy = min(prices[i], coins / i);
+
+            count += canbuy;
+            coins -= canbuy * i;
+
+            if (coins < i)
+                break;
         }
         return count;
     }
