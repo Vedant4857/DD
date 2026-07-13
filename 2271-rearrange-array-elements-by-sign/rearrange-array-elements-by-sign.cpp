47867f7b7fdd837b3331d814fbe48eee8eaf840c
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-07-13 12:25:43 +0530

    Time: 10 ms (31.80%) | Memory: 134.2 MB (28.89%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+
+        int i = 0, j = 0;
+
+        while (i < n && j < n) {
+            while (nums[i] < 0 && i < n) {
+                i++;
+            }
+            if (i < n && nums[i] > 0) {
+                ans.push_back(nums[i]);
+                i++;
+            }
+
+            while (nums[j] > 0 && j < n) {
+                j++;
+            }
+            if (j < n && nums[j] < 0) {
+                ans.push_back(nums[j]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};
