ecde9e2c8735a2feb184eb9b11529b8c15d51122
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-08-19 21:53:59 +0530

    Time: 2118 ms (5.00%) | Memory: 775.2 MB (5.74%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalways(int n, int amount, vector<int> coins,
+                  vector<vector<int>>& dp) {
+        if (amount == 0) {
+            return 1;
+        }
+        if (n == 0 || amount < 0) {
+            return 0;
+        }
+        if (dp[n][amount] != -1) {
+            return dp[n][amount];
+        }
+        return dp[n][amount] = totalways(n - 1, amount, coins, dp) +
+                               totalways(n, amount - coins[n - 1], coins, dp);
+    }
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        return totalways(n, amount, coins, dp);
+    }
+};
