ebf961deb3140cc5204a4f9482e03084c5f86bb9
Vedant Shekhar <153383069+Vedant4857@users.noreply.github.com>
2026-08-21 22:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (53.52%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<long long> dp(target + 1, 0);
+        vector<int> dp(target + 1, 0);
         dp[0] = 1;
 
         for (int i = 1; i <= target; i++) {
             for (int num : nums) {
                 if (i >= num) {
-                    dp[i] += dp[i - num];
-
-                    // Prevent overflow
-                    if (dp[i] > INT_MAX) {
-                        dp[i] = INT_MAX;
+                    if (dp[i] <= INT_MAX - dp[i - num]) {
+                        dp[i] += dp[i - num];
                     }
                 }
             }
         }
 
-        return (int)dp[target];
+        return dp[target];
     }
 };
